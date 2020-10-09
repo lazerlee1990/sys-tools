f6d2f5799ff14900ea42215719838fa28ef1c301
bob_monday <bob_monday@outlook.com>
2020-10-09 12:14:55 +0100

    latest

--- src/compnents/SideNav/SideNav.jsx
+++ src/compnents/SideNav/SideNav.jsx
@@ -39,12 +39,12 @@ export default function SideNav() {
 
   return (
     <nav >
-        <img style={{maxHeight: "150px", maxWidth: "150px"}} src='/logo192.png' alt='logo' />
+        <img className="logo" style={{padding: "20px"}} src='/logo192.png' alt='logo' />
         <div className="button-menu">
-            <Link style={{background: `${active === '/' ? "#61dafb" : ""}`}} to='/' onClick={() => handleClick("home")}><HomeIcon />Home </Link>
-            <Link style={{background: `${active === '/system' ? "#61dafb" : ""}`}} to='/system' onClick={() => handleClick("system")}><SettingsSystemDaydreamIcon /> System Logs</Link>
-            <Link style={{background: `${active === '/apache' ? "#61dafb" : ""}`}} to='/apache' onClick={() => handleClick("apache")}><LanguageIcon />Apache 2 Data</Link>
-            <Link style={{background: `${active === '/cron' ? "#61dafb" : ""}`}} to='/cron' onClick={() => handleClick("cron")}><AccessAlarmIcon />Cron Jobs</Link>
+            <Link style={{background: `${active === '/' ? "#61dafb" : ""}`}} to='/' onClick={() => handleClick("home")}><HomeIcon style={{padding: "7px"}}/>Home </Link>
+            <Link style={{background: `${active === '/system' ? "#61dafb" : ""}`}} to='/system' onClick={() => handleClick("system")}><SettingsSystemDaydreamIcon style={{padding: "7px"}}/> System Logs</Link>
+            <Link style={{background: `${active === '/apache' ? "#61dafb" : ""}`}} to='/apache' onClick={() => handleClick("apache")}><LanguageIcon style={{padding: "7px"}}/>Apache 2 Data</Link>
+            <Link style={{background: `${active === '/cron' ? "#61dafb" : ""}`}} to='/cron' onClick={() => handleClick("cron")}>{<AccessAlarmIcon style={{padding: "7px"}}/>}  Cron Jobs</Link>
         </div>
     </nav>
   );
