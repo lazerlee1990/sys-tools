859e48cab61ead505df0a04f26485d03dfb6e087
bob_monday <bob_monday@outlook.com>
2020-10-09 18:47:21 +0100

    latest

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Dashboard.css';
 
+
 export default function Dashboard() {
     return (
         <div className="container">
-            <h1>Dashboard</h1>
+            <div className="top-cards-container">
+               
+               
+            </div>
         </div>
     )
 }
+ 
